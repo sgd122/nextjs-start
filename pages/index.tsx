b07b9800c2652f17f2a32b64da7a1928c9d92b78
Seong Gi Dong <sgd0947@gmail.com>
2022-07-17 23:42:26 +0900

    💎 style(main): movie type 지정

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,35 @@ import Seo from "../components/Seo";
 
 const API_KEY = "834be7165bcfa4410f4caefbf916cc51";
 
+interface IMovieApiResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: IMovie[];
+}
+
+interface IMovie {
+  poster_path?: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path?: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
 const Home: NextPage = () => {
-  const [movies, setMovies] = useState<any>();
+  const [movies, setMovies] = useState<IMovie[]>();
   useEffect(() => {
     (async () => {
-      const { results } = await (
+      const { results }: { results: IMovie[] } = await (
         await fetch(
           `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
         )
@@ -22,13 +46,11 @@ const Home: NextPage = () => {
     <div>
       <Seo title={"Home"} />
       {!movies && <h4>Loading...</h4>}
-      {movies?.map(
-        (movie: { id: number; original_title: string; title: string }) => (
-          <div key={movie.id}>
-            <h4>{movie.original_title}</h4>
-          </div>
-        )
-      )}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 };
